b27c0deb1cc3e308e492720779db58c5fb9bf639
KondzioBondzio <kp1105@o2.pl>
2025-07-09 12:44:47 +0200

    :sparkles: Add spatie permisions and Laravel Policies

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Create permissions
+        $permissions = [
+            // Matches permissions
+            'matches.view.any',
+            'matches.create',
+            'matches.update',
+            'matches.delete',
+
+            // User permissions
+            'user.view.any',
+            'user.create',
+            'user.update',
+            'user.delete',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        // Create roles and assign permissions
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo(Permission::all());
+
+        $userRole = Role::create(['name' => 'user']);
+        $userRole->givePermissionTo([
+            'matches.view.any',
+        ]);
+    }
+}
